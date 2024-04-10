92091376b58122db09484c194fd2148cb459577c
snavemail <liam.jk.evans@gmail.com>
2024-04-10 11:34:54 -0400

    Styling

--- src/Integrations/Strava/StravaConnect.tsx
+++ src/Integrations/Strava/StravaConnect.tsx
@@ -1,42 +1,41 @@
-import { Navigate } from 'react-router';
-import { FitCoinState } from '../../store';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import './Strava.css';
+import { Navigate } from "react-router";
+import { FitCoinState } from "../../store";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "./Strava.css";
 
 function StravaConnect() {
   const isLoggedIn = useSelector(
-    (state: FitCoinState) => state.userReducer.isLoggedIn
+    (state: FitCoinState) => state.userReducer.isLoggedIn,
   );
   const stravaId = useSelector(
-    (state: FitCoinState) => state.userReducer.stravaId
+    (state: FitCoinState) => state.userReducer.stravaId,
   );
 
   const handleStravaConnect = async () => {
-    window.location.href = 'http://localhost:4000/api/strava/login';
+    window.location.href = "http://localhost:4000/api/strava/login";
   };
 
   if (!isLoggedIn) {
     return <Navigate to="/login" replace />;
-  } else if (stravaId !== '') {
+  } else if (stravaId !== "") {
     // If they already have a Strava account, redirect to the Home
     return <Navigate to="/home" replace />;
   } else {
     return (
       <div className="strava-wrapper">
-        <h1>Connect To Strava</h1>
+        <h1 className="strava-header">Strava Profile</h1>
         <div className="button-wrapper">
           <button
             onClick={handleStravaConnect}
             id="strava-connect-link"
-            className="btn btn-primary"
+            className="btn-strava"
           >
-            Connect with Strava
+            Connect To Strava
           </button>
-          <p className="or">OR</p>
-          <Link to="/home">
-            <button className="btn btn-primary">Set up later in profile</button>
-          </Link>
+          <p className="or">
+            or <Link to="/home">Set up later in profile</Link>
+          </p>
         </div>
       </div>
     );
